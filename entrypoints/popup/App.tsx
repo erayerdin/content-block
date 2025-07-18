ace0dfd5ae032f6dc112cd6f313205ea0adf30ff
Eray Erdin <eraygezer.94@gmail.com>
2025-07-19 00:34:21 +0300

    init heroui
    Squashed commit of the following:
    
    commit 998c934876b53d249e71b05d9bc37691f9569319
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Sat Jul 19 00:25:00 2025 +0300
    
        configure hero ui
    
    commit 39efc233b965e834e766ccdc07b24794edcb6772
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Sat Jul 19 00:24:43 2025 +0300
    
        impl ChildrenProps
    
    commit 85ac50ba75aa57504dea8b1fcbd8e29d0d05acb6
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Fri Jul 18 23:54:35 2025 +0300
    
        hoist heroui
    
    commit 8ee3f8ae6b3e2917f625acf3f2a6ecb8d39403a9
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Fri Jul 18 23:53:35 2025 +0300
    
        install @heroui/react and framer-motion

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@heroui/react";
+
 function App() {
-  return <div className="text-2xl underline">Hello world.</div>;
+  return (
+    <div className="w-96 h-96 p-4">
+      <Button>Hello world</Button>
+    </div>
+  );
 }
 
 export default App;
