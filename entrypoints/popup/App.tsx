32ceb327fde3c8fe51ea5529bcf9b0c886650e03
Eray Erdin <eraygezer.94@gmail.com>
2025-07-19 23:30:54 +0300

    design popup page
    Squashed commit of the following:
    
    commit 3a2a11960c37f755174aed78371849c6b3865399
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Sat Jul 19 23:30:38 2025 +0300
    
        design popup
    
    commit 3ee27cd5dbaceb7d836bf85743bd54576b8d8ab3
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Sat Jul 19 23:14:27 2025 +0300
    
        install lucide-react

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,9 +1,30 @@
-import { Button } from "@heroui/react";
+import { Tooltip } from "@heroui/react";
+import { PowerIcon, SparklesIcon } from "lucide-react";
 
 function App() {
+  const [url, setURL] = useState<URL>(new URL("https://www.foobar.com"));
+
   return (
-    <div className="w-96 h-96 p-4">
-      <Button>Hello world</Button>
+    <div className="flex flex-col items-stretch justify-between h-full">
+      <div />
+      <div className="flex flex-col items-stretch p-4">
+        <div className="flex flex-col gap-4 items-center">
+          <PowerIcon color="green" size={60} />
+          <p>{url.hostname}</p>
+        </div>
+      </div>
+      <div className="flex gap-4 justify-center bg-gray-300 py-2">
+        <Tooltip content="View and edit filters.">
+          <button
+            onClick={() => {
+              // TODO: Implement this
+            }}
+            type="button"
+          >
+            <SparklesIcon size={20} />
+          </button>
+        </Tooltip>
+      </div>
     </div>
   );
 }
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -8,7 +8,9 @@ import App from "./App.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalProvider>
-      <App />
+      <div className="size-96 max-w-96 max-h-96">
+        <App />
+      </div>
     </GlobalProvider>
   </React.StrictMode>
 );
