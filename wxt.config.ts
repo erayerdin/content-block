0c642840fea924fac7a20fe89d035361d6c6e792
Eray Erdin <eraygezer.94@gmail.com>
2025-07-29 20:20:30 +0300

    use extension icon
    Squashed commit of the following:
    
    commit c120b2018c6a14e8142cd76c4c951f9294c20897
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Tue Jul 29 20:20:04 2025 +0300
    
        define extension icon
    
    commit ab7930f246e66b078d93a999d3bf36df51577900
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Tue Jul 29 20:19:56 2025 +0300
    
        remove default icon
    
    commit eb357140fc52d7c8b4eb2c1da4c9f7707fe88235
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Tue Jul 29 20:16:16 2025 +0300
    
        install @wxt-dev/auto-icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,10 +4,14 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  autoIcons: {
+    baseIconPath: "./assets/img/icon-default.svg",
+    grayscaleOnDevelopment: false,
+  },
   manifest: {
     permissions: ["storage"],
   },
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   vite: () => ({
     plugins: [ViteToml(), tailwindcss()],
   }),
